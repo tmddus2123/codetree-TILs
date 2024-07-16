ae434f2612a80444cdd95f00808be300be9c91b9
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-16 13:49:43 +0900

    Title: 반복 출력하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240716/반복 출력하기/repeated-output.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        print(num);
+    }
+
+    public static void print(int num) {
+        for(int i=0; i<num; i++) {
+            System.out.println("12345^&*()_");
+        }
+    }
+}

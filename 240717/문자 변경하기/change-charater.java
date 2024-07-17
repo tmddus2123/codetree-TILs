dc2747284a26b0db9bb599333254240dfb280858
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-17 15:44:03 +0900

    Delete 240717 directory

--- 240717/문자 변경하기/change-charater.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String a = "c";
-        a = "T";
-
-        System.out.print(a);
-    }
-}
--- 240717/변수 출력하기 3/outputing-variables-3.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String a = "1";
-        String b = "2";
-        String c = "C";
-
-        System.out.print(a + "->" + b + "->" + c);
-    }
-}

080239577d5b0e3e7af7a794c115841468995933
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-17 14:06:50 +0900

    Title: 데이터 교환, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240717/데이터 교환/exchange-data.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String a = "5";
+        String b = "6";
+        String c = "7";
+
+        String nb = a;
+        String nc = b;
+        String na = c;
+
+        System.out.print(na + "\n" + nb + "\n" + nc);
+    }
+}

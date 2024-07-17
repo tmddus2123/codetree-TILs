fccde04a1e66c870ed18749c0539fa5ff7fc258a
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-17 14:35:27 +0900

    Title: 합과 차의 나눗셈, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240717/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+
+        System.out.printf("%.2f", (a+b) / (a-b));
+    }
+}

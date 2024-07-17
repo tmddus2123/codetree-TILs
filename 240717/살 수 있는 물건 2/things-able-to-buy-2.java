89b66b5a759c3e0227d7a7325a4645a14ed8deed
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-17 15:05:05 +0900

    Title: 살 수 있는 물건 2, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240717/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        String thing = "no";
+        
+        if(num >= 3000) {
+            thing = "book";
+        } else if(num >= 1000) {
+            thing = "mask";
+        } else if(num >= 500) {
+            thing = "pen";
+        }
+
+        System.out.print(thing);
+    }
+}

e1831ecc4f9f35517abc273845cae8427ebffd3e
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-19 11:15:46 +0900

    Title: 공약수의 유무, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240719/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int result = 0;
+        for(int i=a; i<=b; i++) {
+            if(1920 % i == 0 && 2880 % i == 0) {
+                result = 1;
+                break;
+            }
+        }
+        System.out.print(result);
+    }
+}

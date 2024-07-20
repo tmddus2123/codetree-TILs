b2af9950698133fdc9b09743c5729fc441a84232
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-20 16:16:13 +0900

    Title: 직각 삼각형 출력, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240720/직각 삼각형 출력/Right-triangle-and-output.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        for(int i=0; i<num; i++) {
+            for(int j=0; j<i*2 + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}

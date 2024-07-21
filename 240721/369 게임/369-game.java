98618a2e5bce659c8ea4057e724fb1010c1a3009
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-21 12:19:26 +0900

    Title: 369 게임, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240721/369 게임/369-game.java
+++ 240721/369 게임/369-game.java
@@ -1,22 +1,29 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int num = sc.nextInt();
+        int num = Integer.parseInt(br.readLine());
+
         for(int i=1; i<=num; i++) {
+            int result = i;
             if(i % 3 == 0) {
-                System.out.print("0 ");
+                bw.write("0 ");
             } else if(String.valueOf(i).indexOf("3") > -1) {
-                System.out.print("0 ");
+                bw.write("0 ");
             } else if(String.valueOf(i).indexOf("6") > -1) {
-                System.out.print("0 ");
+                bw.write("0 ");
             } else if(String.valueOf(i).indexOf("9") > -1) {
-                System.out.print("0 ");
+                bw.write("0 ");
             } else {
-                System.out.print(i + " ");
+                bw.write(i + " ");
             }
         }
+
+        br.close();
+        bw.close();
     }
 }

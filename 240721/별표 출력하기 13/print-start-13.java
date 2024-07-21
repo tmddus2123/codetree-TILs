770629e10777f248ab9edc3dbb8c1c9dc004ad00
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-21 12:39:11 +0900

    Title: 별표 출력하기 13, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240721/별표 출력하기 13/print-start-13.java
+++ 240721/별표 출력하기 13/print-start-13.java
@@ -5,17 +5,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
 
-        int count = 0;
         for(int i=0; i<num; i++) {
             for(int j=0; j<num-i; j++) {
                 System.out.print("* ");
-                count++;
             }
             System.out.print("\n");
 
             for(int j=0; j<=i; j++) {
                 System.out.print("* ");
-                count++;
             }
             System.out.print("\n");
         }

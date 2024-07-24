6fecdf48e5ea2c2f2d41eda7e15c320aa8fd2481
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-24 14:27:48 +0900

    Title: 합을 옆으로 밀어 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240724/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int sum = 0;
+        
+        for(int i=0; i<num; i++) {
+            sum += sc.nextInt();
+        }
+        System.out.print(result.substring(1) + result.substring(0, 1););
+    }
+}

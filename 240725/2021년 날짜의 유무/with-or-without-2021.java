aea818c5c7f63f89e387ceddfcd3614f6818b64c
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-25 15:43:40 +0900

    Title: 2021년 날짜의 유무, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 240725/2021년 날짜의 유무/with-or-without-2021.java
+++ 240725/2021년 날짜의 유무/with-or-without-2021.java
@@ -20,13 +20,10 @@ public class Main {
                 return true;
             } else if((month == 4 || month == 6 || month == 9 || month == 11) && day <= 30) {
                 return true;
-            } else if(day <= 31) {
+            } else if((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && day <= 31) {
                 return true;
-            } else {
-                return false;
             }
-        } else {
-            return false;
         }
+        return false;
     }
 }

25466b7bd7bd8b0573359ca8890728f9df6c7285
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-25 17:48:53 +0900

    Title: 그 계절, 그 날, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240725/그 계절, 그 날/that-season-that-day.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+
+        if(day <= limitDay(year, month, day)) {
+            if(month >= 3 && month <= 5) {
+                System.out.print("Spring");
+            } else if(month >= 6 && month <= 8) {
+                System.out.print("Summmer");
+            } else if(month >= 9 && month <= 11) {
+                System.out.print("Fall");
+            } else {
+                System.out.print("Winter");
+            }
+        }
+    }
+
+    public static int limitDay(int year, int month, int day) {
+        if(month == 2) {
+            if(isLeapYear(year)) {
+                return 29;
+            } else {
+                return 28;
+            } 
+        } else if(month == 4 || month == 6 || month == 9 || month == 11) {
+            return 30;
+        }
+        return 31;
+    }
+
+    public static boolean isLeapYear(int year) {
+        if(year % 100 == 0 && year % 400 != 0){
+            return false;
+        } else if(year % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

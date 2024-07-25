b30adc14805e817aedf35a20cae76ca02162b37b
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-25 13:41:27 +0900

    Title: 함수를 이용한 소수 판별, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240725/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 240725/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -13,6 +13,9 @@ public class Main {
         System.out.print(sum);
     }
     public static int isPrime(int n) {
+        if(n == 1) {
+            return 0;
+        }
         for(int i=2; i<=Math.sqrt(n); i++) {
             if(n % i == 0) {
                 return 0;

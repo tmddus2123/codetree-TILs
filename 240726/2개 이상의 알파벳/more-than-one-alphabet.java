914a8bf03a601dce1818abcad1d808b12cd87e27
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-26 11:25:34 +0900

    Title: 2개 이상의 알파벳, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240726/2개 이상의 알파벳/more-than-one-alphabet.java
+++ 240726/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -14,23 +14,11 @@ public class Main {
     }
 
     public static boolean charNumber(String str) {
-        int[] arr = new int[26];
-
         for(int i=0; i<str.length(); i++) {
-            if(arr[str.charAt(i) - 'a'] == 0) {
-                arr[str.charAt(i) - 'a'] = 1;
+            if(str.charAt(i) != str.charAt(0)) {
+                return true;
             }
         }
-
-        int total = 0;
-        for(int i=0; i<arr.length; i++) {
-            total += arr[i];
-        }
-
-        if(total >= 2) {
-            return true;
-        } else {
-            return false;
-        }
+        return false;
     }
 }

5664e4e087fcf38e75a8348309e86c84098dbb6d
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-26 10:44:55 +0900

    Title: 두 정수 값 교환하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240726/두 정수 값 교환하기/to-exchange-two-integer-values.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = new int [2];
+        arr[0] = sc.nextInt();
+        arr[1] = sc.nextInt();
+
+        arr = swap(arr);
+        System.out.print(arr[0] + " " + arr[1]);
+    }
+
+    public static int[] swap(int[] arr) {
+        int temp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = temp;
+
+        return arr;
+    }
+}

6a92bacabb04f47e5e4ad8c7cd50c081b03a8b56
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-29 14:05:54 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240729/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.print(recur(num));
+    }
+
+    public static int recur(int n) {
+        if(n == 1) {
+            return 0;
+        }
+
+        if(n % 2 == 0 ) {
+            n /= 2;
+        } else {
+            n = n*3 + 1;
+        }
+
+        return 1 + recur(n);
+    }
+}

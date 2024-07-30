0c8e65aada59ae47391449d9c85fdd7c491d8268
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-30 14:50:28 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 231ms, Memory: 10MB, Status: Passed - CodeTree

--- 240730/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
+++ 240730/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -1,30 +1,41 @@
 import java.util.Scanner;
+
 public class Main {
-    public static int commonNumber(int[] arr, int n, int common, int cnt) {
-        
-        for(int i = 0; i < n; i++){
-            if(common % arr[i] == 0) { //최소 공배수가 배열로 나눠지면 cnt++
-                cnt++;
-            }
+    public static final int MAX_N = 10;
+    
+    public static int n;
+    public static int[] arr = new int[MAX_N + 1];
+    
+    // 두 수간의 최소공배수를 구하여 반환합니다.
+    public static int lcm(int a, int b) {
+        int gcd = 1;
+        for(int i = 1; i <= Math.min(a, b); i++) {
+            if(a % i == 0 && b % i == 0)
+                gcd = i;
         }
-        if(cnt == n) { // cnt 가 배열의 갯수와 같아지면 최소공배수 반환
-                return common;
-        }
-        cnt = 0; //루프 다 돌았으면 카운트 0으로 초기화
-        return commonNumber(arr, n, (common + 1), cnt); // 최소 공배수 1씩 늘려가면서 재귀
+    
+        return a * b / gcd;
     }
+    
+    // index번째 까지 인덱스의 숫자 중에 가장 큰 값을 반환합니다.
+    public static int getLCMAll(int index) {
+        // 남은 원소가 1개라면 그 자신이 답이 됩니다.
+        if(index == 1)
+            return arr[1];
+    
+        // 1번째 ~ index - 1번째 원소의 최소공배수를 구한 결과와
+        // 현재 index 원소의 최소공배수를 구하여 반환합니다.
+        return lcm(getLCMAll(index - 1), arr[index]);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // 배열의 갯수
-        int arr[] = new int[n]; // 배열
-        int common = 0; // 최소공배수
-        int cnt = 0;
-        for(int i = 0; i < n; i++){
-            arr[i] = sc.nextInt(); // 최소공배수 초기화
-            if(common < arr[i]){ // 배열중에 제일 큰 걸로 최소공배수 초기화
-                common = arr[i];
-            }
-        }
-        System.out.print(commonNumber(arr, n, common, cnt));
+        // 변수 선언 및 입력:
+        n = sc.nextInt();
+        for(int i = 1; i <= n; i++)
+            arr[i] = sc.nextInt();
+
+        // 모든 수의 최소공배수를 구합니다.
+        System.out.print(getLCMAll(n));
     }
 }

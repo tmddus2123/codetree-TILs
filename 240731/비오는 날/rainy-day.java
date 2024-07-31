6a38d46412029de00dc016e75879695dfe1f35ac
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-07-31 21:11:18 +0900

    Title: 비오는 날, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240731/비오는 날/rainy-day.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Weather {
+    String date;
+    String day;
+    String data;
+
+    public Weather(String date, String day, String data) {
+        this.date = date;
+        this.day = day;
+        this.data = data;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        Weather[] w = new Weather[num];
+
+        for(int i=0; i<num; i++) {
+            String date = sc.next();
+            String day = sc.next();
+            String data = sc.next();
+
+            w[i] = new Weather(date, day, data);
+        }
+
+        int last = 0;
+        for(int i=1; i<num; i++) {
+            if(w[i].data.equals("Rain") && w[i].date.compareTo(w[last].date) < 0) {
+                last = i;
+            }
+        }
+        System.out.print(w[last].date + " " + w[last].day + " " + w[last].data);
+    }
+}

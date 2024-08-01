7a59434734f76d69e27236fb929e87d28e16203b
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-08-01 16:47:48 +0900

    Title: 줄 세우기 2, Time: 273ms, Memory: 15MB, Status: Passed - CodeTree

--- 240801/줄 세우기 2/line-up-students-2.java
+++ 240801/줄 세우기 2/line-up-students-2.java
@@ -12,7 +12,7 @@ class Student implements Comparable<Student>{
     @Override
     public int compareTo(Student s) {
         if(this.height == s.height) {
-            return s.weight - this.height;
+            return s.weight - this.weight;
         } else {
             return this.height - s.height;
         }
@@ -32,6 +32,8 @@ public class Main {
 
             s[i] = new Student(h, w, i+1);
         }
+
+        Arrays.sort(s);
 
         for(Student ss : s) {
             System.out.println(ss.height +  " " + ss.weight + " " + ss.number);

8ce1fccc37c895b4e252aa46eca808c638364589
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-08-01 16:44:45 +0900

    Title: 줄 세우기 2, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240801/줄 세우기 2/line-up-students-2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Student implements Comparable<Student>{
+    int height, weight, number;
+
+    public Student(int height, int weight, int number) {
+        this.height = height;
+        this.weight = weight;
+        this.number = number;
+    }
+
+    @Override
+    public int compareTo(Student s) {
+        if(this.height == s.height) {
+            return s.weight - this.height;
+        } else {
+            return this.height - s.height;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        Student[] s = new Student[num];
+
+        for(int i=0; i<num; i++) {
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+
+            s[i] = new Student(h, w, i+1);
+        }
+
+        for(Student ss : s) {
+            System.out.println(ss.height +  " " + ss.weight + " " + ss.number);
+        }
+    }
+}

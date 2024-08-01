85dbb503976cd936b590aa839f1682eaea951ce9
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-08-01 15:37:49 +0900

    Title: 원점으로부터의 거리, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240801/원점으로부터의 거리/distance-from-origin.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Position implements Comparable<Position>{
+    int x, y, dis, number, rank;
+
+    public Position(int x, int y, int dis, int number) {
+        this.x = x;
+        this.y = y;
+        this.dis = dis;
+        this.number = number;
+    }
+
+    public void toRank(int rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public int compareTo(Position p) {
+        if(this.dis == p.dis) {
+            return p.number - this.number;
+        }
+        return p.dis - this.dis ;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        Position[] p = new Position[num];
+
+        for(int i=0; i<num; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            
+            x = (x > 0) ? x : -x;
+            y = (y > 0) ? y : -y; 
+
+            p[i] = new Position(x, y, x+y, i+1);
+        }
+
+        Arrays.sort(p);
+
+        for(int i=0; i<num; i++) {
+            p[i].rank = i+1;
+        }
+
+        Arrays.sort(p, new Comparator<Position>() {
+            @Override
+            public int compare(Position a, Position b) {
+                return a.number - b.number;
+            }
+        });
+
+        for(int i=0; i<num; i++) {
+            System.out.println(p[i].rank);
+        }
+    }
+}

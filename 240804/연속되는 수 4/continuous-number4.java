ee1a796fa222e21a21285e740339917f6a6c699d
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-08-04 13:46:47 +0900

    Title: 연속되는 수 4, Time: 183ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240804/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int prev = sc.nextInt();
+        int count = 1, max = 1;
+        for(int i=1; i<num; i++) {
+            int cur = sc.nextInt();
+            
+            if(prev < cur) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if(max < count) {
+                max = count;
+            }
+            prev = cur;
+        }
+        System.out.print(max);
+    }
+}

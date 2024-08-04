468b6cbe0e85fc1c8ae41ce4a9e5313265e3bc73
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-08-04 14:16:48 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240804/만나는 그 순간/the-moment-we-meet.java
+++ 240804/만나는 그 순간/the-moment-we-meet.java
@@ -11,46 +11,53 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
 
+        int total1 = 0;
         int time = 0;
         int pos = 0;
         for(int i=0; i<n; i++) {
             String dir = sc.next();
             int t = sc.nextInt();
+            total1 += t;
 
             if(dir.equals("L")) {
-                while(t > 0) {
-                    arr1[time++] = pos++;
-                    t--;
-                }
-            } else {
                 while(t > 0) {
                     arr1[time++] = pos--;
                     t--;
                 }
+            } else {
+                while(t > 0) {
+                    arr1[time++] = pos++;
+                    t--;
+                }
             }
+            
         }
 
+        int total2 = 0;
         time = 0;
         pos = 0;
         for(int i=0; i<m; i++) {
             String dir = sc.next();
             int t = sc.nextInt();
+            total2 += t;
 
             if(dir.equals("L")) {
-                while(t > 0) {
-                    arr2[time++] = pos++;
-                    t--;
-                }
-            } else {
                 while(t > 0) {
                     arr2[time++] = pos--;
                     t--;
                 }
+            } else {
+                while(t > 0) {
+                    arr2[time++] = pos++;
+                    t--;
+                }
             }
+            
         }
 
         int result = -1;
-        for(int i=1; i<1001; i++) {
+
+        for(int i=1; i<Math.max(total1, total2); i++) {
             if(arr1[i] == arr2[i]) {
                 result = i;
                 break;

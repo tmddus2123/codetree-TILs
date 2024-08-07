0784e25ebe1ee5b034faf1087d4e0d3fef1e265e
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-08-07 15:11:51 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 230ms, Memory: 12MB, Status: Passed - CodeTree

--- 240807/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
+++ 240807/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
@@ -2,48 +2,59 @@ import java.util.*;
 
 public class Main {
 
-    public static int[] dx = new int[]{1, 0, -1, 0};
-    public static int[] dy = new int[]{0, 1, 0, -1};
+    static int n;
+    static int m;
+    static int[][] arr;
 
-    static int n, m;
+    static int r = 0; //행값
+    static int c = 0; //열값
+    static int d = 1; //방향값 초기값 오른쪽
+
+    static final int[] dx = new int[]{-1, 0, 1, 0};
+    static final int[] dy = new int[]{0, 1, 0, -1};
+
+    //범위안에 있나?
+    static boolean inRange(){
+        if((r + dx[d] >= 0 && r + dx[d] < n) && (c + dy[d] >= 0 && c + dy[d] < m)){
+            return true;
+        }
+        return false;
+    }
+
+    //이미 지나갔던곳이 아닌가? = 다음칸이 0인가?
+    static boolean isPossible(){
+        return arr[r + dx[d]][c + dy[d]] == 0;
+    }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
 
-        int[][] arr = new int[n][m];
+        n = sc.nextInt(); //행
+        m = sc.nextInt(); //열
+        arr = new int[n][m];
 
-        int x = 0, y = 0;
-        int dir = 0;
-        int count = 2;
-
-        arr[0][0] = 1;
-
-
-        while(count <= n * m) {
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-
-            if(!inRange(nx, ny) || arr[nx][ny] != 0) {
-                dir = (dir + 1) % 4;
+        arr[r][c] = 1;
+        for(int i = 2; i <= n * m; i++){
+            if(inRange() && isPossible()){
+                r += dx[d];
+                c += dy[d];
+                arr[r][c] = i;
+            }else{
+                d = (d + 1) % 4;
+                if(inRange() && isPossible()){
+                    r += dx[d];
+                    c += dy[d];
+                    arr[r][c] = i;
+                }
             }
-
-            x += dx[dir];
-            y += dy[dir];
-            arr[x][y] = count;
-            count++;
         }
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                System.out.print(arr[j][i] + " ");
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j] + " ");
             }
-            System.out.println();
-        }       
-    }
-
-    private static boolean inRange(int x, int y) {
-        return (0 <= x && x < n && 0 <= y && y < m);
+            System.out.println("");
+        }
     }
 }

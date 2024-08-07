d234273f0056534b1739343327b4d5bb7450ab43
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-08-07 15:29:09 +0900

    Title: 되돌아오기, Time: 155ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240807/되돌아오기/come-back.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+
+    public static int[] dx = {1, 0, -1, 0};
+    public static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int[][] map = new int[2001][2001];
+
+        int x = 1000;
+        int y = 1000;
+        map[x][y] = -1;
+        
+        int count = 1;
+        int result = -1;
+
+        for(int i=1; i<=num; i++) {
+            String dir = sc.next();
+            int dis = sc.nextInt();
+
+            int d = 0;
+            if(dir.equals("E")) {
+                d = 0;
+            } else if(dir.equals("N")) {
+                d = 1;
+            } else if(dir.equals("W")) {
+                d = 2;
+            } else {
+                d = 3;
+            }
+
+            for(int j=0; j<dis; j++) {
+                x += dx[d];
+                y += dy[d];
+
+                if(map[x][y] == -1) {
+                    result = count;
+                    break;
+                }
+                map[x][y] = count;
+                count++;
+            }
+        }
+
+        System.out.print(result);
+    }
+}

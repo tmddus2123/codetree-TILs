d21cf66fac005776153e5c6fc0c74f5152188099
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-08-09 23:19:18 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240809/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.java
+++ 240809/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.java
@@ -12,7 +12,7 @@ public class Main {
 
         int max = arr[0] + arr[2];
         for(int i=0; i<num - 2; i++) {
-            for(int j=i + 2; i<num; i++) {
+            for(int j=i + 2; j<num; j++) {
                 max = Math.max(max, arr[i] + arr[j]);
             }
         }

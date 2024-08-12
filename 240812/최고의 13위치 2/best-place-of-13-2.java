23381c2d13b2ba436ddf9faa9351eb5f691b20d3
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-08-12 17:22:04 +0900

    Title: 최고의 13위치 2, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240812/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int num = sc.nextInt();
+        int[][] arr = new int[num][num];
+
+        for(int i=0; i<num; i++) {
+            for(int j=0; j<num; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int x = 0, y = 0;
+        int max1 = 0;
+        for(int i=0; i<num; i++) {
+            for(int j=0; j<num-2; j++) {
+                if(max1 < arr[i][j] + arr[i][j+1] + arr[i][j+2]) {
+                    max1 = arr[i][j] + arr[i][j+1] + arr[i][j+2];
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+
+        int max2 = 0;
+        for(int i=0; i<num; i++) {
+            for(int j=0; j<num-2; j++) {
+                if(i == x && (j == y || j == y+1 || j == y+2)) {
+                    continue;
+                }
+                max2 = Math.max(max2, arr[i][j] + arr[i][j+1] + arr[i][j+2]);
+            }
+        }
+        System.out.print(max1 + max2);
+    }
+}

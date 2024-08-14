f6cbf9b017065d80205d4de183cf45bf1ee73f67
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-08-14 14:41:13 +0900

    Title: G or H 2, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240814/G or H 2/G-or-H-2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int[] arr = new int[101];
+
+        for(int i=0; i<num; i++) {
+            int n = sc.nextInt();
+            String str = sc.next();
+
+            int a = (str.equals("G")) ? 1 : 2;
+            arr[n] = a;
+        }
+
+        int max = 0;
+        for(int i=1; i<101; i++) {
+
+            int g = 0, h = 0;
+            for(int j=i; j<101; j++) {
+                if(arr[j] != 0) {
+                    if(arr[j] == 1) {
+                        g++;
+                    } else {
+                        h++;
+                    }
+                    if(g == h) {
+                        max = Math.max(max, j - i - 1);
+                    }
+                }
+
+            }
+        }
+        System.out.print(max);
+    }
+}

0bbdf7cdd75899cc0fd311b161fb9ac621c5cfa8
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-08-20 19:24:13 +0900

    Title: 스승의 은혜 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240820/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int budget = sc.nextInt();
+        
+        int[] price = new int[n];
+        int[] fee = new int[n];
+
+        for(int i=0; i<5; i++) {
+            price[i] = sc.nextInt();
+            fee[i] = sc.nextInt();
+        }
+
+        int max = 0;
+        for(int i=0; i<5; i++) {
+            int[] salePrice = price.clone();
+
+            salePrice[i] = salePrice[i]/2;
+            
+            for(int j=0; j<5; j++) {
+                salePrice[j] += fee[j];
+            }
+
+            Arrays.sort(salePrice);
+            int sum = 0;
+            int count = 0;
+            for(int j=0; j<5; j++) {
+                sum += salePrice[j];
+                if(sum > budget) {
+                    break;
+                }
+                count++;
+            }
+            if(max < count) {
+                max = count;
+            }
+        }
+        System.out.print(max);
+    }
+}

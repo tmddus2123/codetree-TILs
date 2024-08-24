e10eb9aa42b058b60400adadf91da5f7e0204d7d
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-08-24 14:44:55 +0900

    Title: 팰린드롬 수 찾기, Time: 515ms, Memory: 121MB, Status: Passed - CodeTree

--- 240824/팰린드롬 수 찾기/find-the-number-of-palindrome.java
+++ 240824/팰린드롬 수 찾기/find-the-number-of-palindrome.java
@@ -1,29 +1,37 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    public static int x, y;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 입력
+        x = sc.nextInt();
+        y = sc.nextInt();
 
-        int x = sc.nextInt();
-        int y = sc.nextInt();
+        int ans = 0;
+        // 각 정수에 대해 
+        // 팰린드롬 수인지 아닌지 여부를 판단합니다.
+        for(int i = x; i <= y; i++) {
+            // 정수 형태를 문자열 배열로 바꿉니다.
+            String str = Integer.toString(i);
 
-        int count = 0;
-        for(int i=x; i<=y; i++) {
-            String num = String.valueOf(i);
+            boolean isPalindrome = true;
 
-            int len = num.length();
+            // 펠린드롬 수가 되기 위해서는,
+            // 거꾸로 읽어도 똑같은 수여야 합니다.
+            String strReverse = "";
+            for(int j = str.length() - 1; j >= 0; j--)
+                strReverse += str.charAt(j);
 
-            boolean result = true;
-            for(int j=0; j<len/2; j++) {
-                if(num.charAt(j) != num.charAt(len-j-1)) {
-                    result = false;
-                    break;
-                }
-            }
-            if(result) {
-                count++;
-            }
+            if(!str.equals(strReverse))
+                isPalindrome = false;
+
+            if(isPalindrome)
+                ans++;
         }
-        System.out.print(count);
+        
+        System.out.print(ans);
+        
     }
 }

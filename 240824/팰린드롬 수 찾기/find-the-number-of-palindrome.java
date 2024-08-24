9b818ee1407061506068d1c863d592866a18f71a
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-08-24 14:41:27 +0900

    Title: 팰린드롬 수 찾기, Time: 244ms, Memory: 20MB, Status: Passed - CodeTree

---
+++ 240824/팰린드롬 수 찾기/find-the-number-of-palindrome.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int count = 0;
+        for(int i=x; i<=y; i++) {
+            String num = String.valueOf(i);
+
+            int len = num.length();
+
+            boolean result = true;
+            for(int j=0; j<len/2; j++) {
+                if(num.charAt(j) != num.charAt(len-j-1)) {
+                    result = false;
+                    break;
+                }
+            }
+            if(result) {
+                count++;
+            }
+        }
+        System.out.print(count);
+    }
+}
